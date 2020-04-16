c809f89850796c3f2ad1bdad5eaa7a371320e879
Elaine Portela <elainepportela@gmail.com>
2020-04-16 09:06:18 -0300

    Revert "fix(queries): name query to repo"

--- lib/octo_events_web/controllers/event_controller.ex
+++ lib/octo_events_web/controllers/event_controller.ex
@@ -1,19 +1,27 @@
 defmodule OctoEventsWeb.EventController do
   use OctoEventsWeb, :controller
 
-  alias OctoEvents.EventRepo
+  alias OctoEvents.EventQuery
+  alias OctoEventsWeb.EventView
 
   def create(conn, params) do
-    EventRepo.set_insert_event(params)
+    EventQuery.set_insert_event(params)
     conn
     |> put_resp_content_type("text/plain")
     |> send_resp(200, "")
   end
 
   def show(conn, params) do
-    events = EventRepo.get_by_issue_id(params["issue_id"])
-    IO.inspect(events)
-    render(conn, "show.json", events: events)
+    EventQuery.get_events_by_issue_id(params["issue_id"])
+    |> List.first()
+    |> EventView.event_to_json()
+    |> pretty_json(conn)
+  end
+
+  def pretty_json(data, conn) do
+    conn
+    |> Plug.Conn.put_resp_header("content-type", "application/json; charset=utf-8")
+    |> Plug.Conn.send_resp(200, Poison.encode!(data, pretty: true))
   end
 
 end
--- lib/octo_events_web/views/event_view.ex
+++ lib/octo_events_web/views/event_view.ex
@@ -2,20 +2,11 @@ defmodule OctoEventsWeb.EventView do
   use OctoEventsWeb, :view
 
   def issue_to_json(issue) do
-    issue
-    |> Map.take([:title, :state, :created_at, :closed_at, :body])
+    Map.take(issue, [:title, :state, :created_at, :closed_at, :body])
   end
 
-  def render("show.json", %{events: events}) do
-    %{data: render_many(events, OctoEventsWeb.EventView, "event.json")}
+  def event_to_json(event) do
+    Map.take(event, [:issue_id, :action])
+    |> Map.put_new(:issue, issue_to_json(event.issue))
   end
-
-  def render("event.json", %{event: event}) do
-    %{
-      action: event.action,
-      issue_id: event.issue_id,
-      issue: issue_to_json(event.issue),
-    }
-  end
-
 end
